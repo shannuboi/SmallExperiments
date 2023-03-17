32cdca259750e87652d1d369ca133c666a1fe720
shannu boi <shayan.khan.xyz@gmail.com>
2023-03-18 00:00:12 +0500

    Moved stuff from main to header file

--- SmallExperiments/FasterMultiplication.h
+++ SmallExperiments/FasterMultiplication.h
@@ -2,6 +2,11 @@
 
 #include <algorithm>
 #include <math.h>
+#include <iostream>
+#include <random>
+#include <vector>
+#include <functional>
+#include <chrono>
 
 template <class T>
 int GetNumOfDigits(const T& num, int base = 10)
@@ -54,6 +59,45 @@ T KaratsubaMultiply(const T& a, const T& b, int base)
 	{
 		return a * b;
 	}
+}
+
+template <typename T>
+std::vector<T> GenarateArray(int SIZE, std::mt19937& rng, T smallest = (T)0, T largest = (T)100)
+{
+	std::uniform_int_distribution<T> dist(smallest, largest);
+	std::vector<T> arr(SIZE);
+
+	for (T& n : arr) n = dist(rng);
+
+	return arr;
+}
+
+int RunTest()
+{
+	std::random_device rd;
+	std::mt19937 rng;
+	const int SIZE = 10000;
+	std::vector<long long> arr1 = std::move(GenarateArray(SIZE, rng, (long long)1000000000000, (long long)1000000000000000));
+	std::vector<long long> arr2 = std::move(GenarateArray(SIZE, rng, (long long)1000000000000, (long long)1000000000000000));
+
+	auto before = std::chrono::steady_clock::now();					// Mark time b4
+	for (int i = 0; i < std::min(arr1.size(), arr2.size()); i++)
+	{
+		KaratsubaMultiply(arr1[i], arr2[i]);
+	}
+	auto after = std::chrono::steady_clock::now();					// Mark time after
+
+	std::chrono::duration<double> deltaT = after - before;		// Diff is time taken
+	std::cout << "Time taken Karatsuba multiplication: " << deltaT.count() << "\n\n";
 
 
+	before = std::chrono::steady_clock::now();						// Mark time b4
+	for (int i = 0; i < std::min(arr1.size(), arr2.size()); i++)
+	{
+		arr1[i] * arr2[i];
+	}
+	after = std::chrono::steady_clock::now();						// Mark time after
+
+	deltaT = after - before;										// Diff is time taken
+	std::cout << "Time taken regular multiplication: " << deltaT.count() << "\n\n";
 }
