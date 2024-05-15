3fef754e345bfe642b26b3377e0a7e5aad418545
shannu boi <shayan.khan.xyz@gmail.com>
2024-05-15 22:18:49 +0500

    More smal changes

--- SmallExperiments/Staganalysis.h
+++ SmallExperiments/Staganalysis.h
@@ -12,7 +12,7 @@ int runmain() {
 	const unsigned char special_byte_3v1 = 0x8c;
 	const unsigned char special_byte_3v2 = 0x8d;
 
-	std::ifstream f("B.txt");
+	std::ifstream f("steg_text.txt");
 	bool isCypheringBit = false;
 	std::deque<unsigned char> buffer = { 0,0 };
 
@@ -27,11 +27,16 @@ int runmain() {
 
 		std::cout << "Char: " << c << '\t' << "bits: " << binary << '\t' << "hex: " << hex;
 
+		if (buffer[2] == special_byte_1 || buffer[2] == special_byte_2 ||
+			buffer[2] == special_byte_3v1 || buffer[2] == special_byte_3v2)
+		{
+			std::cout << "\t*";
+		}
+
 		if ( buffer[0] == special_byte_1 && buffer[1] == special_byte_2 &&
 			(buffer[2] != special_byte_3v1 || buffer[2] != special_byte_3v2)) 
 		{
 			msgBinary += (buffer[2] & 1) ? '1' : '0';
-			std::cout << "\t(Extra)";
 		}
 		
 		std::cout << '\n';
